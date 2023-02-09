d2aae323157ccb4d5357ad3650417fd33b623e09
sehee-bang <i0475833@naver.com>
2023-02-09 11:36:33 +0900

    up

---
+++ src/components/TodoMain.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="page">
+    <header><h1>Vue Fire todo1</h1></header>
+    <main>
+      <div class="todos">
+        <div class="write">
+          <input type="text"/>
+          <button class="btn add">Add</button>
+        </div>
+        <ul class="list">
+          <li>
+            <i class="far fa-check-square"></i>
+            <span>
+              원고 챕터1 마감
+              <b>
+                <a href="">Edit</a>
+                <a href="">Del</a>
+              </b>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </main>
+    </div>
+</template>
+
+<script>
+export default {
+    todos:[
+        {text: '공부하기', state: 'yet'},
+        {text: '운동하기', state: 'done'},
+        {text: '글쓰기', state: 'done'},
+      ]
+}
+</script>
+
+<style>
+
+</style>
